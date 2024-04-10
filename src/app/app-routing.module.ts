afea88ce28536c6a608adcd0e10636e89ab9a2c5
sergio <sergio.ume@hotmail.com>
2024-04-10 09:25:33 +0100

    cradas las entidades y sus rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { UsuarioComponent } from './component/usuario/usuario.component';
+import { ArticuloComponent } from './component/articulo/articulo.component';
+import { IniciarSesionComponent } from './component/iniciar-sesion/iniciar-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'usuario', component: UsuarioComponent },
+  { path: 'articulo', component: ArticuloComponent },
+  { path: 'iniciar-sesion', component: IniciarSesionComponent },
+  // Redirecciona cualquier ruta no encontrada a Home, opcionalmente puedes crear un componente para Página no encontrada
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { UsuarioComponent } from './component/usuario/usuario.component';
+import { ArticuloComponent } from './component/articulo/articulo.component';
+import { IniciarSesionComponent } from './component/iniciar-sesion/iniciar-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsuarioComponent,
+    ArticuloComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
